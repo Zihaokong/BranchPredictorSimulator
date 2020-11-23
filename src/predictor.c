48f75a0247f329fcdb9cba210851687daded0c47
EC2 Default User <ec2-user@ip-172-31-21-215.us-east-2.compute.internal>
2020-11-23 23:54:25 +0000

    finihs

--- src/predictor.c
+++ src/predictor.c
@@ -8,11 +8,22 @@
 #include <stdio.h>
 #include "predictor.h"
 #include<string.h>
+#include<math.h>
+
 #define MAX 3
 #define MIN 0
 #define INCREMENT(ct) ({ if((ct)->counter != MAX) (ct)->counter++;})
 #define DECREMENT(ct) ({ if((ct)->counter != MIN) (ct)->counter--;})
+#define ABS(X) ((X)>=0? (X) : -1*(X))
 #define PRED_MASK 2
+
+
+
+#define WIDTH 16
+#define HEIGHT 146
+#define LENGTH 7
+
+
 
 //
 // TODO:Student Information
@@ -39,6 +50,7 @@ int verbose;
 //      Predictor Data Structures     //
 //------------------------------------//
 
+// 2 bit sarturating counter struct
 typedef struct {
   char counter;
 }SatCounter;
@@ -48,13 +60,20 @@ typedef struct {
 //        Predictor Functions         //
 //------------------------------------//
 
-
+/* branch history table for gshare and 
+global predictor in tournament  */
 SatCounter* BHTGlobal;
 uint32_t globalHistory;
 
-SatCounter* BHTLocal;
-uint32_t* PHTLocal;
 
+
+/* local branch pattern history table 
+and associate branch history table for 
+pattern history. */
+uint32_t* PHTLocal;
+SatCounter* BHTLocal;
+
+/* meta predictor in tournament predictor*/
 SatCounter* META;
 
 
@@ -70,31 +89,31 @@ void init_global_predictor(int BHTStartValue, SatCounter** BHT){
 
 
 uint8_t Gshare_predict(uint32_t pc){
+  // calculate index into BHT global using XOR
   uint32_t indexedPC = pc % (1<<ghistoryBits);
   uint32_t BHTIndex = indexedPC ^ globalHistory;
 
+  // get prediction bit
   uint8_t counter = BHTGlobal[BHTIndex].counter;
   return (counter & PRED_MASK) >> 1;  
 }
 
 
 void Gshare_train(uint32_t pc, uint8_t outcome){
+  //calculating index into BHT global using XOR
   uint32_t indexedPC = pc % (1<<ghistoryBits);
   uint32_t BHTIndex = indexedPC ^ globalHistory;
   uint32_t outcome_padd = outcome;
 
-  if(outcome == 1) INCREMENT(BHTGlobal+BHTIndex);     //increment(BHT+BHTIndex,3);
-  else DECREMENT(BHTGlobal+BHTIndex);                                         //decrement(BHT+BHTIndex,0); 
+  // change saturating counter based on branch outcome
+  if(outcome == 1) INCREMENT(BHTGlobal+BHTIndex);  
+  else DECREMENT(BHTGlobal+BHTIndex);       
 
+  //leftshift glbal history for new branch outcome.
   globalHistory = globalHistory<<1;
   globalHistory = globalHistory % (1<<ghistoryBits);
   globalHistory = globalHistory | outcome_padd;
-
 }
-
-
-
-
 
 
 void init_tour_predictor(int BHTStartValue, SatCounter** BHTGlobal, SatCounter** BHTLocal, uint32_t** PHT, SatCounter** META ){
@@ -114,14 +133,17 @@ void init_tour_predictor(int BHTStartValue, SatCounter** BHTGlobal, SatCounter**
 }
 
 uint8_t Tour_predict(uint32_t pc){
+  // get global prediction with BHTGlobal
   uint8_t global_prediction = (BHTGlobal[globalHistory].counter & PRED_MASK) >> 1;
 
+  // calculating index into local pattern history table and get local branch prediction rsult
   uint32_t indexedPC = pc % (1<<pcIndexBits);
   uint8_t local_prediction = (BHTLocal[PHTLocal[indexedPC]].counter & PRED_MASK) >> 1;
 
+  //get meta predictor result of which result to use.
   uint8_t chooser_prediction = (META[globalHistory].counter & PRED_MASK) >> 1;
 
-
+  // decide which branch to take.
   uint8_t result = (chooser_prediction == 0)? global_prediction : local_prediction;
   return result;
 
@@ -129,34 +151,111 @@ uint8_t Tour_predict(uint32_t pc){
 
 
 void Tour_train(uint32_t pc, uint8_t outcome){
+  // get global, local prediction from BHT
   uint8_t global_prediction = (BHTGlobal[globalHistory].counter & PRED_MASK) >> 1;
   uint32_t indexedPC = pc % (1<<pcIndexBits);
   uint8_t local_prediction = (BHTLocal[PHTLocal[indexedPC]].counter & PRED_MASK) >> 1;
 
   // train meta predictor
-  if(global_prediction == outcome && local_prediction != outcome) DECREMENT(META+globalHistory);                                        //decrement(META + globalHistory, 0);
-  else if(local_prediction == outcome && global_prediction != outcome) INCREMENT(META+globalHistory);   //increment(META + globalHistory, 3);
+  if(global_prediction == outcome && local_prediction != outcome) DECREMENT(META+globalHistory);                    
+  else if(local_prediction == outcome && global_prediction != outcome) INCREMENT(META+globalHistory);   
 
 
-  // train global predictor
+  // increase BHT in global predictor, and update global history register
   uint32_t outcome_padd = outcome;
-  if(outcome == 1)  INCREMENT(BHTGlobal+globalHistory);                                                       //increment(BHT+globalHistory,3);
-  else DECREMENT(BHTGlobal+globalHistory);                                                                      //decrement(BHT+globalHistory,0); 
+  if(outcome == 1)  INCREMENT(BHTGlobal+globalHistory);                                      
+  else DECREMENT(BHTGlobal+globalHistory);                                                       
 
   globalHistory = globalHistory<<1;
   globalHistory = globalHistory % (1<<ghistoryBits);
   globalHistory = globalHistory | outcome_padd;
 
-  // train local predictor
+  // update branch history table in local predictor
   uint32_t patternHistory = PHTLocal[indexedPC];
-  if(outcome == 1)  INCREMENT(BHTLocal+patternHistory);                                                  //increment(BHTLocal+patternHistory,3);
-  else DECREMENT(BHTLocal+patternHistory);                                   //decrement(BHTLocal+patternHistory,0); 
+  if(outcome == 1)  INCREMENT(BHTLocal+patternHistory);                                  
+  else DECREMENT(BHTLocal+patternHistory);                                   
 
+  // update pattern history table with new branch outcome
   patternHistory = patternHistory << 1;
   patternHistory = patternHistory % (1<< lhistoryBits);
   patternHistory = patternHistory | outcome_padd;
-
   PHTLocal[indexedPC] = patternHistory;
+}
+
+//threshold
+int8_t theta;
+//history register with the same number of weights for each perceptron.
+int8_t customHistory[WIDTH];
+//table of perceptrons with bias
+int16_t weights[HEIGHT][WIDTH+1];
+
+
+void init_perceptron_predictor(){
+  // formula from https://www.cs.utexas.edu/~lin/papers/hpca01.pdf
+  theta = (int8_t)(1.93 * WIDTH + 14);
+  // initialize bias and weights for each perceptron in table.
+  for(int i = 0;i<HEIGHT;i++){
+    //bias = 1;
+    weights[i][0] = 1;
+    for(int j = 1;j<WIDTH+1;j++){
+      weights[i][j] = 0;
+    }
+  }
+  // initialize history register with NOTTAKEN
+  memset(customHistory,0,WIDTH*sizeof(int16_t));
+}
+
+
+int16_t Custom_predict(uint32_t pc){
+
+  //calculatiing index and get bias from perceptron table
+  uint32_t indexedPC = pc % HEIGHT;
+  int16_t y = weights[indexedPC][0];
+  // return the result of dot product
+  for(int i = 1;i<WIDTH+1;i++){
+    y += customHistory[i-1] ? weights[indexedPC][i] : (-1)*weights[indexedPC][i];
+  }
+  return y;
+}
+
+
+
+void shiftHistory(char outcome){
+  //shift in the outcome to LSB of history register 
+  for(int i = 1; i < WIDTH; i ++){
+    customHistory[i-1] = customHistory[i];
+  }
+  customHistory[WIDTH-1] = outcome;
+}
+
+
+
+void Custom_train(uint32_t pc, uint8_t outcome){
+  //calculating index 
+  uint32_t indexedPC = pc % HEIGHT;
+
+  //calculating branch result
+  int16_t y = Custom_predict(pc);
+  int result;
+  if(y>=0) result = TAKEN;
+  else result = NOTTAKEN; 
+
+  // if result is different from label, or result is less than threshold, train the weights.
+  if((result != outcome) || ABS(y) <= theta ){
+    //train bias
+    weights[indexedPC][0] += ((outcome == 1)? 1 : -1) * 1;
+    //train weights
+    for(int i = 1; i <= WIDTH;i++){
+      //wi = wi + t*x
+      int16_t predict = (customHistory[i-1] == 0)? -1:1;
+      int16_t outcome_signed = (outcome == 0)? -1 : 1;
+      if(weights[indexedPC][i] != ((1<<(LENGTH-1))-1) && weights[indexedPC][i] != -(1 <<(LENGTH-1)))
+        weights[indexedPC][i] += outcome_signed*predict;
+
+    }
+  }
+  //shift left with LSB the new outcome
+  shiftHistory(outcome);
 }
 
 
@@ -174,6 +273,9 @@ init_predictor()
       return;
     case TOURNAMENT:;
       init_tour_predictor(WN,&BHTGlobal,&BHTLocal,&PHTLocal,&META);
+      return;
+    case CUSTOM:;
+      init_perceptron_predictor();
       return;
   }
 }
@@ -196,7 +298,9 @@ uint8_t make_prediction(uint32_t pc)
     case TOURNAMENT:
       //call predictor method
       return Tour_predict(pc);
-    case CUSTOM:
+    case CUSTOM:;
+      int16_t y = Custom_predict(pc);
+      return (y>=0) ? TAKEN : NOTTAKEN;
     default:
       break;
   }
@@ -222,7 +326,8 @@ train_predictor(uint32_t pc, uint8_t outcome)
     case TOURNAMENT:;
       Tour_train(pc, outcome);
       return;
-
+    case CUSTOM:
+      Custom_train(pc,outcome);
     default:
       break;
   }
